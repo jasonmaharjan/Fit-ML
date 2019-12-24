d7b34204270c74f2d7c8d9dd4867edf4020e62fc
Jason <jason8maharjan@gmail.com>
2019-12-24 08:16:46 +0545

    Data Preprocessing

--- project.py
+++ project.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer  
 
-dataset_1 = pd.read_csv('ACTIVITY_1576296000247.csv').drop_duplicates()
+dataset_1 = pd.read_csv('ACTIVITY_1577030100133.csv').drop_duplicates()
 dataset_1['date'] = dataset_1.date.astype('datetime64[ns]')
-dataset_9 = pd.read_csv('USER_1576296000151.csv')
+dataset_9 = pd.read_csv('USER_1577030100069.csv')
 
 
 # Average calculation
 
-date_hb = pd.read_csv('HEARTRATE_AUTO_1576296000585.csv').drop(['time'], axis = 1)
+date_hb = pd.read_csv('HEARTRATE_AUTO_1577030100366.csv').drop(['time'], axis = 1)
 date_hb.to_csv('HEARTRATE.csv', index = False)
 
 df = pd.read_csv('HEARTRATE.csv', parse_dates = ['date'], index_col = 'date')
@@ -20,15 +20,17 @@ avg = avg.reset_index()
 # Merge datasets
 
 merge = dataset_1.merge(avg, on = 'date')
+merge['lastSyncTime'] = pd.to_datetime(merge['lastSyncTime'],unit='s')
 merge.to_csv("merge.csv")
 
 
 # For missing Values (nan)
-Z = merge.iloc[:, 3:].values
+Z = merge.iloc[:, 2:].values
 
 imputer = SimpleImputer(missing_values = np.nan, strategy = "mean")
 imputer = imputer.fit(Z[:,:])
 Z = (imputer.transform(Z[:,:]))
+
 
 
 
