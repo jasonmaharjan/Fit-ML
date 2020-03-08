71cf212b7e1d9402308ac23b0f0484be8119778b
Jason <jason8maharjan@gmail.com>
2020-03-08 17:36:40 +0545

    Graphs added

--- project_2.py
+++ project_2.py
@@ -1,62 +1,148 @@
-# Data Preprocessing
+# COMPARISON OF FITNESS BANDS
 
 # MI band
+
+# Data Preprocessing
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.impute import SimpleImputer  
 
 # Importing Datasets
-dataset_1 = pd.read_csv('ACTIVITY_s.csv').drop_duplicates()
-dataset_1['date'] = dataset_1.date.astype('datetime64[ns]')
-dataset_2 = pd.read_csv('SLEEP_s.csv').drop_duplicates()
-# dataset_9 = pd.read_csv('USER_1580696400122.csv')
+dataset_activity = pd.read_csv('ACTIVITY_a.csv').drop_duplicates()
+dataset_activity['date'] = dataset_activity.date.astype('datetime64[ns]')
+dataset_sleep = pd.read_csv('SLEEP_a.csv').drop_duplicates()
 
 # Average heartbeat calculation
-date_hb = pd.read_csv('HEARTRATE_s.csv').drop(['time'], axis = 1)
+date_hb = pd.read_csv('HEARTRATE_a.csv').drop(['time'], axis = 1)
 date_hb.to_csv('HEARTRATE.csv', index = False)
 hb = pd.read_csv('HEARTRATE.csv', parse_dates = ['date'], index_col = 'date')
-avg_hb = hb.resample('D').mean()
+avg_hb = hb.resample('D').mean()    
 avg_hb = avg_hb.reset_index()
 
-# Merge average heartbeat to main dataset
-merged = dataset_1.merge(avg_hb, on = 'date')
-merged['lastSyncTime'] = pd.to_datetime(merged['lastSyncTime'],unit='s')
-merged = merged.drop(['lastSyncTime'], axis = 1)
-merged.to_csv("merge.csv")
+# Merge average heartbeat/day to main dataset
+hb_merged = dataset_activity.merge(avg_hb, on = 'date')
+hb_merged['lastSyncTime'] = pd.to_datetime(hb_merged['lastSyncTime'],unit='s')
+hb_merged = hb_merged.drop(['lastSyncTime'], axis = 1)
+hb_merged.to_csv("hb_merge.csv")
 
-# Merging sleep cycle to main dataset
-dataset_2['date'] = dataset_2['date'].astype('datetime64[ns]')
-deep_sleep_merge = merged.merge(dataset_2, on = 'date').drop('lastSyncTime', axis =1)
-deep_sleep_merge['start'] = pd.to_datetime(deep_sleep_merge['start'], unit='s')
-deep_sleep_merge['stop'] = pd.to_datetime(deep_sleep_merge['stop'], unit='s')
+# Merging dataset_sleep to dataset_activity
+dataset_sleep['date'] = dataset_sleep['date'].astype('datetime64[ns]')
+dataset_merged = hb_merged.merge(dataset_sleep, on = 'date').drop('lastSyncTime', axis =1)
+dataset_merged['start'] = pd.to_datetime(dataset_merged['start'], unit='s')
+dataset_merged['stop'] = pd.to_datetime(dataset_merged['stop'], unit='s')
 
-"""
-# For missing Values ( 0 and nan)
-X = deep_sleep_merge.iloc[:,1:5].values
+
+# For 0 values in hb_merged (activity columns)
+x = hb_merged.iloc[:,1:5].values
 imputer_zero = SimpleImputer(missing_values = 0, strategy = "mean")
-imputer_zero = imputer_zero.fit(X)
-X = (imputer_zero.transform(X))
-X = X.astype('int64')
-X = np.array(X)
+imputer_zero = imputer_zero.fit(x)
+x = (imputer_zero.transform(x))
+x = x.astype('int64')
+x = np.array(x)
 
-Z = deep_sleep_merge.iloc[:,5:6].values
+# For nan values in hb_merged (heartbeat column)
+y = hb_merged.iloc[:,5:6].values
 imputer_nan = SimpleImputer(missing_values = np.nan, strategy = "mean")
-imputer_nan = imputer_nan.fit(Z[:,:])
-Z = (imputer_nan.transform(Z[:,:]))
-Z = Z.astype('int64')
-Z = np.array(Z)
+imputer_nan = imputer_nan.fit(y[:,:])
+y = (imputer_nan.transform(y[:,:]))
+y = y.astype('int64')
+y = np.array(y)
 
-final_dataset = np.concatenate((X,Z), axis=1)
-final_dataset = pd.DataFrame(final_dataset)
-"""
-
+# Dataframe with 0 and nan values removed
+z = np.concatenate((x,y), axis=1)
+z = pd.DataFrame(z)
 
 
 # Machine Learning
 
-X = deep_sleep_merge.iloc[:, 1:4].values # Independent variable matrix
-Y = deep_sleep_merge.iloc[:, 4].values # Dependent variable martix
+X = z.iloc[:, :3].values # Independent variable matrix
+Y = z.iloc[:, 3].values # Dependent variable martix
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0) 
+
+# Fitting Multiple Linear Regression into Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+# Predicting the test set results
+Y_pred = regressor.predict(X_test)
+Y_pred = Y_pred.astype('int64')
+
+
+# Backward Elimination Algorithm
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((X.shape[0], 1)).astype(int), values = X, axis = 1)  # first column must have value 1 for b0 constant
+
+# Removing independent variables that are statistically insignificant
+X_opt = X[:,[0,1,2]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit() 
+regressor_OLS.summary()
+
+X_opt = X[:,[0,2]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+# Most Significant Feature
+X_opt = X[:,[1]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+# Visualising the regression results
+
+# Steps VS Calories
+plt.scatter(X[:,1], Y, color = 'red')
+plt.plot(X[:,1], regressor_OLS.predict(X[:,1]), color = 'blue')
+plt.title('Fitness Analysis')
+plt.xlabel('Steps')
+plt.ylabel('Calories')
+plt.show()
+
+# Distance VS Calories
+regressor_OLS = sm.OLS(endog = Y, exog = X[:,[2]]).fit()
+plt.scatter(X[:,2], Y, color = 'red')
+plt.plot(X[:,2], regressor_OLS.predict(X[:,2]), color = 'blue')
+plt.title('Fitness Analysis')
+plt.xlabel('Distance')
+plt.ylabel('Calories')
+plt.show()
+
+# 3D graph (Multiple Regression)
+
+fig = plt.figure()
+ax = fig.add_subplot(111,projection="3d")
+ax.scatter(dataset_merged['distance'], dataset_merged['steps'], dataset_merged['calories'], c='blue', marker = 'o', s = 10)
+ax.view_init(30, 185)
+plt.xlabel('Distance')
+plt.ylabel('Steps')
+plt.zlabel('Calories')
+plt.show()
+
+
+
+
+# Fitbit 
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+fitbit_dataset = pd.read_csv('fitbit_activity_a.csv')
+fitbit_dataset['Steps'] = fitbit_dataset["Steps"].str.replace(",","").astype(float)
+fitbit_dataset['Date'] = fitbit_dataset.Date.astype('datetime64[ns]')
+fitbit_dataset['Calories'] = fitbit_dataset["Calories"].str.replace(",","").astype(float)
+fitbit_dataset['Distance'] = fitbit_dataset['Distance'] * 1000
+
+X = fitbit_dataset.iloc[:, 1:3].values # Independent variable matrix
+Y = fitbit_dataset.iloc[:, 3].values # Dependent variable martix
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -76,12 +162,8 @@ import statsmodels.formula.api as sm
 X = np.append(arr = np.ones((X.shape[0], 1)).astype(int), values = X, axis = 1)  # first column must have value 1 for b0 constant
 
 # Removing independent variables that are statistically insignificant
-X_opt = X[:,[0,1,2,3]]
-regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit() 
-regressor_OLS.summary()
-
 X_opt = X[:,[0,1,2]]
-regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit() 
 regressor_OLS.summary()
 
 X_opt = X[:,[0,1]]
@@ -93,29 +175,37 @@ X_opt = X[:,[1]]
 regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
 regressor_OLS.summary()
 
+
 # Visualising the regression results
-plt.scatter(X_opt, Y, color = 'red')
-plt.plot(X_opt, regressor_OLS.predict(X_opt), color = 'blue')
-plt.title('Fitness Analysis')
+
+# Distance VS Calories
+plt.scatter(X[:,1], Y, color = 'red')
+plt.plot(X[:,1], regressor_OLS.predict(X[:,1]), color = 'blue')
+plt.title('Distance VS Calories')
+plt.xlabel('Distance')
+plt.ylabel('Calories')
+plt.show()
+
+# Steps VS Calories
+regressor_OLS = sm.OLS(endog = Y, exog = X[:,2]).fit()
+plt.scatter(X[:,2], Y, color = 'red')
+plt.plot(X[:,2], regressor_OLS.predict(X[:,2]), color = 'blue')
+plt.title('Steps VS Calories')
 plt.xlabel('Steps')
 plt.ylabel('Calories')
 plt.show()
 
 
-Y_prediction = regressor_OLS.predict([9500])
-# Fitbit 
-"""
-dataset_3 = pd.read_csv('fitbit.csv')
-dataset_3['Date'] = dataset_3.Date.astype('datetime64[ns]')
+# 3D graph (Multiple Regression)
 
-X2 = dataset_3.iloc[:, 1:2].values # Independent variable matrix
-Y2 = dataset_3.iloc[:, 3].values # Dependent variable martix
-
-
-
-
+fig = plt.figure()
+ax = fig.add_subplot(111,projection="3d")
+ax.scatter(fitbit_dataset['Distance'], fitbit_dataset['Steps'], fitbit_dataset['Calories'], c='blue', marker = 'o', s = 10)
+ax.view_init(30, 185)
+plt.xlabel('Distance')
+plt.ylabel('Steps')
+plt.zlabel('Calories')
+plt.show()
 
 
 
-
-"""
